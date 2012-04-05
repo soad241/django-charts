6fb82d50aaa51b65800efc135164acd67723a7f2
Julio Manuel Alegría <julio.alegira@ucsp.edu.pe>
2012-04-04 20:39:13 -0500

    comma float problem

--- charts/templatetags/chart_tags.py
+++ charts/templatetags/chart_tags.py
@@ -2,6 +2,13 @@ from django import template
 
 register = template.Library()
 
+@register.filter
+def jsfloatformat(value):
+    try:
+        return str(float(jsfloatformat)).replace(',', '.')
+    except ValueError:
+        return value
+
 @register.inclusion_tag('charts/pie_chart_obj_info.html')
 def pie_chart_obj_info(chart, obj):
     return {'title': chart.get_title(obj),
