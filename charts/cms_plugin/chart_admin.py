8ec334ccf7fe29d9232bbe0f53ab5f573fc2e3a5
Julio Manuel Alegría <julio.alegira@ucsp.edu.pe>
2012-04-11 19:41:34 -0500

    register models

--- charts/cms_plugin/chart_admin.py
+++ charts/cms_plugin/chart_admin.py
@@ -1,3 +1,4 @@
+from django.db.models import Model
 from django.core.exceptions import ImproperlyConfigured
 from django.utils.translation import ugettext_lazy as _
 
@@ -6,20 +7,31 @@ from charts import BaseChart
 class ChartPool(object):
     def __init__(self):
         self._charts = {}
+        self._models = []
 
-    def register(self, chart_class):
+    def register_chart(self, chart_class):
         if not issubclass(chart_class, BaseChart):
-            raise ValueError(_('Registered class must be a chart '
-                             '(subclass of BaseChart)'))
+            raise ValueError(_('Registered class must be a subclass '
+                               ' of charts.BaseChart)'))
         if not chart_class.slug:
             raise ImproperlyConfigured(_("Chart class must have a 'slug'"))
         self._charts[chart_class.slug] = chart_class
 
-    def get_choices(self):
+    def get_all_charts(self):
         return [(chart_slug, chart_class.display_name or chart_class.__name__)
                 for chart_slug, chart_class in self._charts.items()]
 
     def get_chart_class(self, chart_slug):
         return self._charts.get(chart_slug)
+
+    def register_model(self, model):
+        if not issubclass(chart_class, Model):
+            raise ValueError(_('Registered model must be a subclass '
+                               'of djando.db.models.Model'))
+        content_type = ContentType.objects.get_for_model(model)
+        self._models.append(content_type.id)
+
+    def get_all_models(self):
+        return self._models
 
 chart_pool = ChartPool()
--- charts/cms_plugin/forms.py
+++ charts/cms_plugin/forms.py
@@ -1,10 +1,12 @@
 from django import forms
+
 from charts.cms_plugin.models import ChartPluginModel
 from charts.cms_plugin import chart_pool
 
-
 class ChartPluginModelForm(forms.ModelForm):
     chart_class = forms.ChoiceField()
+    content_type = forms.ModelChoiceField(queryset=ContentType.objects.none())
+
     class Meta:
         fields = ('chart_class', 'title', 'width', 'height', 'content_type',
                   'object_id')
@@ -12,5 +14,6 @@ class ChartPluginModelForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(ChartPluginModelForm, self).__init__(*args, **kwargs)
-        self.fields['chart_class'].choices = chart_pool.get_choices()
-        
+        self.fields['chart_class'].choices = chart_pool.get_all_charts()
+        self.fields['content_type'].queryset =\
+                ContentType.objects.filter(id__in=chart_pool.get_all_models())
