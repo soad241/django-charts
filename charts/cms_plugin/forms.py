60da42841c229459c82aeee5926b77fd84aa6bd4
Julio Manuel Alegría <julio.alegira@ucsp.edu.pe>
2012-04-11 19:48:32 -0500

    fixes

--- charts/cms_plugin/forms.py
+++ charts/cms_plugin/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.contenttypes.models import ContentType
 
 from charts.cms_plugin.models import ChartPluginModel
 from charts.cms_plugin import chart_pool
