7074c42e621fcf23c9ebf28553fbc4392cd2fae6
Julio Manuel Alegría <julio.alegira@ucsp.edu.pe>
2012-04-19 21:39:25 -0500

    test 3 with json

--- charts/line_chart.py
+++ charts/line_chart.py
@@ -1,4 +1,18 @@
+import json
+
 from base_chart import BaseChart, ImproperlyConfigured, _
+
+def serialize_datetime(dt):
+    """ JSON serialize datetime, date or anything that behaves like them """
+    pattern =\
+        'new Date({year},{month},{day},{hour},{minute},{second},{microsecond})'
+    return pattern.format(year=getattr(dt, 'year', 1990),
+                          month=getattr(dt, 'month', 1)-1,
+                          day=getattr(dt, 'day', 1),
+                          hour=getattr(dt, 'hour', 0),
+                          minute=getattr(dt, 'minute', 0),
+                          second=getattr(dt, 'second', 0),
+                          microsecond=getattr(dt, 'microsecond', 0))
 
 class Period(object):
     def __init__(self, period='', label='', count=None, selected=False):
@@ -60,3 +74,15 @@ class LineChart(BaseChart):
     def get_value(self, obj):
         raise ImproperlyConfigured(_("LineChart requires a definition of "
                                      "'get_value'"))
+    
+    def get_dataset_as_json(self):
+        json_data = []
+        for dataset in self.get_datasets():
+            dataset_data = []
+            for obj in self.get_queryset(dataset):
+                dataset_data.append({
+                                'date': serialize_datetime(self.get_date(obj)),
+                                'value': self.get_value(obj) or 0.0})
+            json_data.append(json.dumps(dataset_data))
+        return json_data
+
