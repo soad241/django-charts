7c729f0fba70f14adb36beb7df8d9c4b4e57b9ee
Julio Manuel Alegría <julio.alegira@ucsp.edu.pe>
2012-04-19 22:42:06 -0500

    fix

--- charts/templatetags/chart_tags.py
+++ charts/templatetags/chart_tags.py
@@ -7,12 +7,12 @@ def xy_chart_get_label_text(chart, obj):
     return chart.get_label_text(obj)
 
 @register.simple_tag
-def xy_chart_get_color(chart, obj, selected=False):
-    return chart.get_color(obj, selected)
-
-@register.simple_tag
 def xy_chart_get_text_color(chart, obj, selected=False):
     return chart.get_text_color(obj, selected)
+
+@register.simple_tag
+def xy_chart_get_color(chart, obj, selected=False):
+    return chart.get_color(obj, selected)
 
 @register.simple_tag
 def line_chart_get_title(chart, dataset):
