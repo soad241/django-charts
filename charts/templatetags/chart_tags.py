855057acc7e2e89453df69d436ad51395f66442c
Julio Manuel Alegría <julio.alegira@ucsp.edu.pe>
2012-04-19 22:02:00 -0500

    more tests

--- charts/templatetags/chart_tags.py
+++ charts/templatetags/chart_tags.py
@@ -2,15 +2,22 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('charts/xy_chart_obj_info.html')
-def xy_chart_obj_info(chart, obj, selected=False):
-    return {'label_text': chart.get_label_text(obj),
-            'color': chart.get_color(obj, selected),
-            'text_color': chart.get_text_color(obj, selected)}
+@register.simple_tag
+def xy_chart_get_label_text(chart, obj):
+    return chart.get_label_text(obj)
 
-@register.inclusion_tag('charts/line_chart_dataset_info.html')
-def line_chart_dataset_info(chart, dataset, div_id, idx):
-    return {'title': chart.get_title(dataset),
-            'color': chart.get_color(dataset),
-            'div_id': div_id,
-            'idx': idx}
+@register.simple_tag
+def xy_chart_get_color(chart, obj, selected=False):
+    return chart.get_color(obj, selected)
+
+@register.simple_tag
+def xy_chart_get_text_color(chart, obj, selected=False):
+    return chart.get_text_color(obj, selected)
+
+@register.simple_tag
+def line_chart_get_title(chart, dataset):
+    return chart.get_title(dataset)
+
+@register.simple_tag
+def line_chart_get_color(chart, dataset):
+    return chart.get_color(dataset)
